a1f2d07a560d7f00ac7b30d2fbe5f46746c60635
pravdishem <praveen.kumar@dishem.com>
2016-08-09 17:26:30 +0530

    test initial commit

--- Merchant/DishemMerchant/DashBoardVC.swift
+++ Merchant/DishemMerchant/DashBoardVC.swift
@@ -15,7 +15,7 @@ class DashBoardVC: UIViewController {
         
         title = "Dashboard"
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.blueColor()]
-        // test comment 
+        // test comment
 
         // Do any additional setup after loading the view.
     }
